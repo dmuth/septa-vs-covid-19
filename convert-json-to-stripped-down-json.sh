91af02f4b4fca7db604bdf8ffd5a8b1d7b463127
Douglas Muth <dmuth@dmuth.org>
2020-04-19 22:00:57 -0400

    Wrote script to strip down train data

---
+++ convert-json-to-stripped-down-json.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# This script reads train data from stdin, and writes out key=value data containing
+# only the timestamp, train number, and minutes late.
+#
+# This is because I'm doing crazy amounts of data processing on my 8-year old iMac
+# and I only have so much CPU and RAM.  If I can do this to avoid paying for some 
+# cloud instances, so much the better.
+#
+
+
+# Errors are fatal
+set -e
+
+>&2 echo "# "
+>&2 echo "# Now reading JSON train data from input and ouputting key=value data..."
+>&2 echo "# "
+
+
+jq -r '._timestamp + " trainno=" + .trainno + " late=" + (.late|tostring) '
+
+
+
+
+
